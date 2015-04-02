eb3f4243ed8aa89c1b31d8d268a2bd1fb6ca4ce6
Victor Perin <victor_perin@nerdhitech.com.br>
2015-04-02 17:22:29 -0300

    Primeira versão que realmente baixa os arquivos...

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
-var request = require("request")
+var 
+	fs = require('fs'),
+    request = require('request');
+	
 var pesquisa = "amazonia brasil";
 var zoom= 13;
+var limiteApi=10;
+var pastaDownload = "imagens/";
+var quantidadeImpressa=0;
 var url = encodeURI("http://maps.google.com/maps/api/geocode/json?address="+pesquisa+"&sensor=false");
 
 request({
@@ -25,20 +31,31 @@ request({
 		//console.log(northeast.lng+" "+northeast.lat);
 		//console.log(southwest.lng+" "+southwest.lat);
 		while(nowLat>=southwest.lat){
-			while(nowLng>=southwest.lng){
+			nowLng=northeast.lng;
+			while(nowLng>=southwest.lng&&quantidadeImpressa<limiteApi){
 				getImage(nowLat,nowLng);
 				nowLng-=getZoomDifference(zoom);
 			}
 			nowLat-=getZoomDifference(zoom);
 		}
+		console.log("Quantidade de imagens baixadas = "+quantidadeImpressa);
 		
     }
 })
 
 
 function getImage(lat,lng){
-	var urlImage = encodeURI("http://maps.googleapis.com/maps/api/staticmap?center="+lat+","+lng+"&size=640x640&maptype=satellite&zoom="+zoom+"&scale=2");
+	var urlImage = encodeURI("http://maps.googleapis.com/maps/api/staticmap?center="+lat+","+lng+"&size=640x640&maptype=satellite&zoom="+zoom+"&format=jpg");
 	console.log(urlImage);
+	
+	quantidadeImpressa++;
+	nomeArquivo = pastaDownload+quantidadeImpressa+'.jpg';
+	download(urlImage,nomeArquivo);
+	
+	
+	
+	
+	
 }
 
 function getZoomDifference(zoom){
@@ -48,3 +65,15 @@ function getZoomDifference(zoom){
 	}
 	return distance;
 }
+
+
+
+
+download = function(uri, filename){
+  request.head(uri, function(err, res, body){
+
+    request(uri).pipe(fs.createWriteStream(filename)).on('close',
+		function(){console.log(filename+" OK!");}
+	);
+  });
+};
